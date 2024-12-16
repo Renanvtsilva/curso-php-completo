38db2e8ad67a92b546c2d30274e8f52e383a89df
RENAN VIANA TEIXEIRA DA SILVA <Renan.Viana@FUTFANATICS.LOCAL>
2024-12-16 14:42:43 -0300

    Passar parâmetros por array

--- callback.php
+++ callback.php
@@ -1,13 +1,7 @@
 <?php
  
-function teste($name)
-{
-    return 'teste' . $name;
+function teste($name,$age){
+    return 'Ola meu nome é ' . $name . ' e minha idade é ' . $age;
 }
 
-function teste2($callback)
-{
-    return call_user_func($callback,'Renan');
-}
-
-echo teste2('teste');
+echo call_user_func_array('teste', ['Renan', '24']);
